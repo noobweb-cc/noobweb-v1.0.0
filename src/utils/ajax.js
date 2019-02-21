96360891f4983dccf1228f391089c7e57899728b
hongfei.tian <hongfei.tian@zhangmen.com>
2019-02-21 18:29:10 +0800

    fix family

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-axios.defaults.baseURL = 'http://vue.cc';
+// axios.defaults.baseURL = 'http://vue.cc';
+axios.defaults.baseURL = 'http://www.noobweb.cc/api/wearefamily';
 
 axios.interceptors.response.use(function (response) {
     return response;
--- src/views/isyours/canbb/index.vue
+++ src/views/isyours/canbb/index.vue
@@ -4,11 +4,18 @@
     </div>
 </template>
 <script>
+import {get, post} from '@/utils/index.js'
+import api from '@/api/icanbb.js'
 export default {
     data () {
         return {
             msg: 'I can BB'
         }
+    },
+    mounted () {
+        get(`${api.sgetList}`).then(res=>{
+            console.log(res)
+        })
     }
 }
 </script>
